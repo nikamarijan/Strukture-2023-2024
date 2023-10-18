ac5875700b01672a838c69905425040ed8f6374f
nikamarijan <nika.marijan656@gmail.com>
2023-10-18 16:35:33 +0200

    1

--- Vjezba1/Vjezba1/Vj1.c
+++ Vjezba1/Vjezba1/Vj1.c
@@ -1,9 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-
-int main() 
-{
-
-	printf("Hello world\n");
-
-	return 0;
+#define MAX_SIZE 50
+#define MAX_LINE 1024
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+} Student;
+int main() {
+	int noRows = 0;
+	FILE* filePointer = NULL;
+	char buffer[MAX_LINE] = { 0 };
+	filePointer = fopen("students.txt", "r");
+	if (!filePointer) {
+		printf("File is not open!\n");
+		return -1;
+	}
+	while (!feof(filePointer)) {
+		fgets(buffer, MAX_LINE, filePointer);
+		noRows++;
+	}
+	printf("%d", noRows);
+	fclose(filePointer);
 }
